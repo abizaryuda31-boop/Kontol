f76b60c2b9a88e58245bdfe5ae7282b04130a3df
abizaryuda31-boop <abizaryuda31@gmail.com>
2026-08-26 17:06:46 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,25 @@
+# config.py
+# =====================================================
+# KONFIGURASI BOT - JOB GMAIL PRO
+# =====================================================
+
+BOT_TOKEN = "8906343611:AAEy_s4sKfn-QlxHGi_LFCx6JYzLxsY0vIo"
+ADMIN_ID = [8483243704]  # List ID Telegram admin
+
+# Channel wajib join (gunakan username dengan @)
+CHANNEL_USERNAME = "@jobgmailterbaruchina"   # Channel Utama
+CHANNEL_MONITOR = "@monitortf"     # Ganti pake username channel bukti TF lu (dengan @)
+
+# URL foto menu utama
+PHOTO_URL = "https://files.catbox.moe/ndjay3.png"
+
+# Aturan bisnis
+MIN_BIRTH_YEAR = 2000
+PRICE_PER_EMAIL = 2000   # Harga beli per Gmail (Rp)
+REFERRAL_BONUS = 200     # Bonus per referral (Rp)
+MIN_WITHDRAW = 5000      # Minimal withdraw (Rp)
+
+# Password wajib untuk Gmail yang disetor (RULES BARU)
+REQUIRED_PASSWORD = "sgsg1122"
+
+DB_FILE = "bot_data.db"
